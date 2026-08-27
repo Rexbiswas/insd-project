583f217cea8e92aa3428ba3c1db2acb84aa507f4
rexbiswas <rexbiswas1@gmail.com>
2026-08-27 17:11:58 +0530

    feat: url shift course/ from aviation/

--- src/app/interior-design-course/page.jsx
+++ src/app/interior-design-course/page.jsx
@@ -1,12 +1,35 @@
 'use client';
 
 import { Suspense } from 'react';
+import Script from 'next/script';
 import PageComponent from '@/views/InteriorDesigning';
 
 export default function Page() {
   return (
-    <Suspense fallback={null}>
-      <PageComponent />
-    </Suspense>
+    <>
+      {/* Google Tag Manager */}
+      <Script id="gtm-interior" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K3KH58B2');
+        `}
+      </Script>
+
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-K3KH58B2"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
+
+      <Suspense fallback={null}>
+        <PageComponent />
+      </Suspense>
+    </>
   );
 }
